da8698279208bae9424670b30cae0465cb61d7a8
Siddharth Madhavan <Siddharth Madhavan>
2017-05-27 16:48:01 +0800

    s

--- soc2017/src/app/home/sidebar/sidebar.component.ts
+++ soc2017/src/app/home/sidebar/sidebar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-
+  courses = ['CS1010', 'CS1020', 'CS1231', 'MA1505', 'MA1506']
   constructor() { }
 
   ngOnInit() {
